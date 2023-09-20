468d28c4b02c7321c6b472dcb41123a87c672b37
YuhanPizza <107896556+YuhanPizza@users.noreply.github.com>
2023-09-20 19:04:27 -0400

    added education template

--- src/components/Education.tsx
+++ src/components/Education.tsx
@@ -1,10 +1,49 @@
+import React from 'react';
+
 const Education = () => {
+  const cardStyle = {
+    width: '1000px', // Set the card's width
+    padding: '20px', // Add padding for spacing
+    boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2)', // Add a shadow for a card-like effect
+  };
+
+  const imageContainerStyle = {
+    display: 'flex', // Display the images horizontally
+    justifyContent: 'space-between', // Add space between the images
+    marginTop: '10px', // Add some spacing below the text
+  };
+
+  const imageStyle = {
+    width: '300px', // Adjust the image width as needed
+    height: 'auto', // Maintain the image aspect ratio
+    border: '4px solid #000',
+    borderRadius: '10px'
+  };
+
   return (
-    <section id="education">
-      <h1>Education</h1>
-      <p>XYZ University</p>
-    </section>
+    <div className="container d-flex justify-content-center align-items-center" style={{ minHeight: '100vh' }}>
+      <div className="card" style={cardStyle}>
+        <h1 style={{textAlign:'center'}}><b>Education</b></h1>
+        <h2><b>Computer Programming, 2022-2023</b></h2>
+        <h2>Seneca College - 1750 Finch Ave E, North York</h2>
+        <ul>
+          <li>President's Honours List: Three consecutive semesters in recognition of outstanding academic achievement</li>
+          <li>Cumulative GPA: 3.9/4.0</li>
+        </ul>
+        {/* Flex container for images */}
+        <div style={imageContainerStyle}>
+          <img src="src/assets/Honors Fall.jpeg" alt="Honors 1" style={imageStyle} />
+          <img src="src/assets/Honors Summer.jpeg" alt="Honors 2" style={imageStyle} />
+          <img src="src/assets/Winter Honors.JPG" alt="Honors 3" style={imageStyle} />
+        </div>
+      </div>
+    </div>
   );
 };
 
 export default Education;
+
+
+
+
+
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -6,13 +6,13 @@ import Projects from "./Projects";
 const Home = () => {
   return (
     <div style={{display:'flex', flexDirection: 'column', alignItems: 'center'}}>
-    <div style={{ marginBottom: '20px', marginTop: '20px' }}>
+    <div style={{ marginTop: '20px' }}>
       <AboutMe />
     </div>
-    <div style={{ marginBottom: '20px', marginTop: '850px' }}>
+    <div style={{ marginTop: '850px' }}>
       <Skills />
     </div>
-    <div >
+    <div style={{ marginTop: '-300px' }}>
       <Education />
     </div>
     <div style={{ marginBottom: '20px' }}>
