accbd31afb32d88476e34306ae98f35d72d50bc9
YuhanPizza <107896556+YuhanPizza@users.noreply.github.com>
2023-09-20 18:22:10 -0400

    improved temporary Home page template

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -6,21 +6,21 @@ import Projects from "./Projects";
 
 const Home = () => {
   return (
-    <div>
-      <div style={{ marginBottom: '20px' }}>
-        <AboutMe />
-      </div>
-      <div style={{ marginBottom: '20px' }}>
-        <Skills />
-      </div>
-      <div style={{ marginBottom: '20px' }}>
-        <Education />
-      </div>
-      <div style={{ marginBottom: '20px' }}>
-        <Projects />
-      </div>
+    <div style={{display:'flex', flexDirection: 'column', alignItems: 'center'}}>
+    <div style={{ marginBottom: '20px', marginTop: '20px' }}>
+      <AboutMe />
     </div>
-  );
+    <div style={{ marginBottom: '20px', marginTop: '850px' }}>
+      <Skills />
+    </div>
+    <div >
+      <Education />
+    </div>
+    <div style={{ marginBottom: '20px' }}>
+      <Projects />
+    </div>
+  </div>
+);
 };
 
 export default Home;
