20527fcbe00bb3d2a3b72e7a54007437f5c92ded
YuhanPizza <107896556+YuhanPizza@users.noreply.github.com>
2023-09-21 11:59:41 -0400

    added hover effect for achievements

--- src/components/Education.tsx
+++ src/components/Education.tsx
@@ -1,23 +1,32 @@
 import React from 'react';
 
 const Education = () => {
-  const cardStyle = {
+  const cardStyle: React.CSSProperties = {
     width: '1000px', // Set the card's width
     padding: '20px', // Add padding for spacing
     boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2)', // Add a shadow for a card-like effect
   };
 
-  const imageContainerStyle = {
+  const imageContainerStyle: React.CSSProperties = {
     display: 'flex', // Display the images horizontally
     justifyContent: 'space-between', // Add space between the images
     marginTop: '10px', // Add some spacing below the text
   };
 
-  const imageStyle = {
+  const imageStyle: React.CSSProperties = {
     width: '300px', // Adjust the image width as needed
     height: 'auto', // Maintain the image aspect ratio
     border: '4px solid #000',
-    borderRadius: '10px'
+    borderRadius: '10px',
+    transition: 'transform 0.2s'
+  };
+  // Function to handle hover effect
+  const handleImageHover = (event:React.MouseEvent<HTMLImageElement>)=> {
+    event.currentTarget.style.transform = 'scale(2)'; // Increase the image size by 10% on hover
+  };
+   // Function to handle hover out effect
+   const handleImageHoverOut = (event: React.MouseEvent<HTMLImageElement>) => {
+    event.currentTarget.style.transform = 'scale(1)'; // Restore the original image size on hover out
   };
 
   return (
@@ -32,9 +41,27 @@ const Education = () => {
         </ul>
         {/* Flex container for images */}
         <div style={imageContainerStyle}>
-          <img src="src/assets/Honors Fall.jpeg" alt="Honors 1" style={imageStyle} />
-          <img src="src/assets/Honors Summer.jpeg" alt="Honors 2" style={imageStyle} />
-          <img src="src/assets/Winter Honors.JPG" alt="Honors 3" style={imageStyle} />
+          <img
+            src="src/assets/Honors Fall.jpeg"
+            alt="Honors 1"
+            style={imageStyle}
+            onMouseEnter={handleImageHover} // Add hover effect on mouse enter
+            onMouseLeave={handleImageHoverOut} // Restore original size on mouse leave
+          />
+          <img
+            src="src/assets/Honors Summer.jpeg"
+            alt="Honors 2"
+            style={imageStyle}
+            onMouseEnter={handleImageHover} // Add hover effect on mouse enter
+            onMouseLeave={handleImageHoverOut} // Restore original size on mouse leave
+          />
+          <img
+            src="src/assets/Winter Honors.JPG"
+            alt="Honors 3"
+            style={imageStyle}
+            onMouseEnter={handleImageHover} // Add hover effect on mouse enter
+            onMouseLeave={handleImageHoverOut} // Restore original size on mouse leave
+          />
         </div>
       </div>
     </div>
